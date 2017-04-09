5de2fd0d8a88064386afac02a9bebfeb9c5386f9
Forest Newark <forest.newark@gmail.com>
2017-04-09 19:05:00 -0400

    Card class javadocs

--- src/main/java/com/theironyard/Card.java
+++ src/main/java/com/theironyard/Card.java
@@ -3,35 +3,18 @@ package com.theironyard;
 
 public class Card {
 
+    //Card Properties
     private String cardName;
     private int cardSkill;
     private int cardTime;
 
 
-    public String getCardName() {
-        return cardName;
-    }
-
-    public void setCardName(String cardName) {
-        this.cardName = cardName;
-    }
-
-    public int getCardSkill() {
-        return cardSkill;
-    }
-
-    public void setCardSkill(int cardSkill) {
-        this.cardSkill = cardSkill;
-    }
-
-    public int getCardTime() {
-        return cardTime;
-    }
-
-    public void setCardTime(int cardTime) {
-        this.cardTime = cardTime;
-    }
-
+    /**
+     * Card Constructor with arguments
+     * @param name of the card
+     * @param skill level of the card
+     * @param time of the card
+     */
     public Card(String name, int skill, int time) {
         cardName = name;
         cardSkill = skill;
@@ -39,17 +22,54 @@ public class Card {
     }
 
 
-
-
-
-
-
-    @Override
-    public String toString() {
-        return "Card{" +
-                "cardName=" + cardName +
-                ", cardSkill=" + cardSkill +
-                ", cardTime=" + cardTime +
-                '}';
+    /**
+     * Get name of the card
+     * @return card name
+     */
+    public String getCardName() {
+        return cardName;
     }
+
+    /**
+     * Set name of the card
+     * @param cardName to be set
+     */
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    /**
+     * Get skill level of the card
+     * @return skill level of card
+     */
+    public int getCardSkill() {
+        return cardSkill;
+    }
+
+    /**
+     * Set skill level of the card
+     * @param cardSkill
+     */
+    public void setCardSkill(int cardSkill) {
+        this.cardSkill = cardSkill;
+    }
+
+    /**
+     * Get time amount for card
+     * @return time for card
+     */
+    public int getCardTime() {
+        return cardTime;
+    }
+
+    /**
+     * Set time amount for card
+     * @param cardTime
+     */
+    public void setCardTime(int cardTime) {
+        this.cardTime = cardTime;
+    }
+
+
+
 }
